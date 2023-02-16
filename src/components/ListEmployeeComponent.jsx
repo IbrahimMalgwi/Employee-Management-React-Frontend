04d67403547ef2ce9579583d8f11891cee6c7467
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-02-16 02:41:43 +0100

    created a componet

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ListEmployeeComponent extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ListEmployeeComponent;
